6320b062e4184a35d2e28dd0192ada2a6ec71df0
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-28 11:57:02 -0500

    -

--- main/cx.esm.js
+++ main/cx.esm.js
@@ -1,7 +1,3 @@
-// deno-fmt-ignore-file
-// deno-lint-ignore-file
-// This code was bundled using `deno bundle` and it's not recommended to edit it manually
-
 "use strict"
 var Dk = Math.imul
 var oSlot = 0
