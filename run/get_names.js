588094d5ab6a836aa7d849458f9e9b92bba5d058
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-28 14:57:04 -0500

    get working with openAi prompt as well

--- run/get_names.js
+++ run/get_names.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env -S deno run --allow-all
-import { getReplacementNames } from "../support/gather_var_renames.js"
+import { getReplacementNames, executeOpenAiPrompt } from "../support/gather_var_renames.js"
 
 await getReplacementNames(
     Deno.readTextFileSync('/Users/jeffhykin/repos/cheerpx_esm/main/cx.esm.js'),
     {
-        onProgress: (index, total, badName, newName)=>console.log(`${(index/total*100).toFixed(2)}% ${index}/${total}, ${badName} -> ${newName}`)
+        onProgress: (index, total, badName, newName)=>console.log(`${(index/total*100).toFixed(2)}% ${index}/${total}, ${badName} -> ${newName}`),
+        executePrompt: executeOpenAiPrompt,
     }
 )
--- support/gather_var_renames.js
+++ support/gather_var_renames.js
@@ -4,13 +4,16 @@ import { Parser, parserFromWasm } from "https://deno.land/x/deno_tree_sitter@0.2
 import javascript from "https://github.com/jeff-hykin/common_tree_sitter_languages/raw/676ffa3b93768b8ac628fd5c61656f7dc41ba413/main/javascript.js"
 import { regex } from 'https://esm.sh/gh/jeff-hykin/good-js@1.14.6.0/source/flattened/regex.js'
 import { frequencyCount } from 'https://esm.sh/gh/jeff-hykin/good-js@1.14.6.0/source/flattened/frequency_count.js'
-
+import OpenAI from "jsr:@openai/openai"
 import $ from "https://esm.sh/jsr/@david/dax@0.42.0/mod.ts"
 var $$ = (...args)=>$(...args).noThrow()
 // await $$`false`
 // await $$`false`.text("stderr")
 // await $$`false`.text("stdout")
 
+const client = new OpenAI({
+    apiKey: Deno.env.get('OPENAI_API_KEY'),
+})
 
 const parser = await parserFromWasm(javascript) // path or Uint8Array
 
@@ -32,22 +35,38 @@ export async function executePromptDefault(prompt) {
     return output
 }
 
+export async function executeOpenAiPrompt(prompt) {
+    const response = await client.responses.create({
+        model: 'gpt-4o-mini',
+        instructions: 'You are a backend API with the skills of a senior developer. You are given a question and you need to answer with a brief formatted answer',
+        input: prompt,
+    })
+    return response.output_text
+}
+
 export function getBadNamesAndContext(code, {filter}={}) {
     const badNames = getBadNames(code, {filter})
     const badNamesAndContexts = {}
     for (let each of badNames) {
-        badNamesAndContexts[each] = [...code.matchAll(regex`((?:(?:\n|^).+){0,5}\\b${each}\\b(?:.+\n){0,5})`.g)].map(match => match[1])
+        badNamesAndContexts[each] = [...code.matchAll(regex`((?:(?:\n|^).+){0,10}\\b${each}\\b(?:.+\n){0,10})`.g)].map(match => match[1])
     }
     return badNamesAndContexts
 }
 
-export async function getReplacementNames(code, {executePrompt=executePromptDefault, maxRetries=3, onProgress=(index, total, badName, newName)=>0, filter=name=>name.length < 3}={}) {
+export function removeWhitespacePrefix(code) {
+    const notAllWhitespaceLines = code.split("\n").filter(each=>!each.match(/^\s*$/))
+    const prefixAmount = Math.min(...notAllWhitespaceLines.map(each=>each.match(/^(\s*)/)[1].length))
+    return notAllWhitespaceLines.map(each=>each.slice(prefixAmount)).join("\n")
+}
+
+export async function getReplacementNames(code, {executePrompt=executePromptDefault, maxRetries=3, onProgress=(index, total, badName, newName)=>0, filter=name=>name.length < 3, contextCap=20}={}) {
     const badNamesAndContexts = getBadNamesAndContext(code, {filter})
     const replacementNames = {}
     let index = 0
     const entries = Object.entries(badNamesAndContexts)
     for (const [name, context] of entries) {
-        const prompt = `Take a look at what "${name}" does in the following snippets. \n\n${context.join('----------------\n')}\n\nPlease suggest a better name for "${name}" and put your answer inside of brackets like this: {{BETTER_NAME}}`
+        const withoutWhitespacePrefix = context.slice(0,contextCap).map(removeWhitespacePrefix)
+        const prompt = `Take a look at what "${name}" does in the following snippets.\n${withoutWhitespacePrefix.map((each,i)=>`snippet example of ${name} #${i+1}:\n\`\`\`\n${each}\n\`\`\`\n`).join("\n")}\n\nPlease suggest a better name for "${name}" and put your answer inside of brackets like this: {{BETTER_NAME}}`
         let newName = name
         let result = await executePrompt(prompt)
         let count = 0
