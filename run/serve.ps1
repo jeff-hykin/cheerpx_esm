3e52126cd778520cd563536f79842a255ec4323b
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-28 11:41:18 -0500

    fix version

--- run/serve.ps1
+++ run/serve.ps1
@@ -5,11 +5,11 @@ echo --% >/dev/null;: ' | out-null
 #
 # for not-Windows operating systems
 #
-deno run --allow-net --allow-read --allow-write --allow-run --allow-sys https://raw.githubusercontent.com/jeff-hykin/archaeopteryx/80316971344a78b32f60708134ecc850b8083102/mod.ts main/
+deno run --allow-net --allow-read --allow-write --allow-run --allow-sys https://raw.githubusercontent.com/jeff-hykin/archaeopteryx/2b2de2a286817637c298bb9c23f102a77c5cde72/mod.ts --allowAbsolute --cors main/
 
 exit #>
 
 #
 # for windows (powershell)
 #
-deno run --allow-net --allow-read --allow-write --allow-run --allow-sys https://raw.githubusercontent.com/jeff-hykin/archaeopteryx/80316971344a78b32f60708134ecc850b8083102/mod.ts main/
+deno run --allow-net --allow-read --allow-write --allow-run --allow-sys https://raw.githubusercontent.com/jeff-hykin/archaeopteryx/2b2de2a286817637c298bb9c23f102a77c5cde72/mod.ts --allowAbsolute --cors main/
